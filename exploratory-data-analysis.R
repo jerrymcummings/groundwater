12a4a47f13e2ab28da2781c5364a24bfa3f3a18a
jerrymcummings <jerrymcummings@gmail.com>
2014-09-11 21:46:11 -0400

    more exploration

--- exploratory-data-analysis.R
+++ exploratory-data-analysis.R
@@ -140,6 +140,7 @@ library(dplyr)
 library(ggplot2)
 library(ggmap)
 library(RColorBrewer)
+library(animation)
 
 DATA_FNAME <- 'water.data.RData'
 
@@ -159,61 +160,176 @@ if (file.exists(DATA_FNAME)) {
 
 water.data.df <- tbl_df(water.data)
 
-# get sites that have at least a year's
-# worth of data
-ok.sites <-
-  water.data.df %>%
-  group_by(site.code) %>%
-  summarize(date.range=as.numeric(max(date.time) - min(date.time))) %>%
-  filter(date.range >= 365)
+if (FALSE) {
+  # number of observations per state
+  table(water.data$state.code)
+}
 
-# get the water data for those sites
-ok.data <-
-  water.data.df %>%
-  filter(site.code %in% ok.sites$site.code) %>%
-  filter(feet.below.surface <= 3000.0)
+if (TRUE) {
+  NBR_SAMPLES <- 3
+  
+  sites <- group_by(water.data.df, site.code)
+  site.obs.counts <- summarize(sites, count=n())
+  summary(site.obs.counts$count)
+  ok.sites <- site.obs.counts %>% filter(count > 200)
+  
+  ok.data <- 
+    water.data.df %>%
+    filter(site.code %in% ok.sites)
+  
+  idx <- sample(1:nrow(ok.sites), size=NBR_SAMPLES, replace=FALSE)
+  sample.site.codes <- ok.sites$site.code[idx]
+  sample.data <-
+    water.data.df %>%
+    filter(site.code %in% sample.site.codes)
+  
+  ggplot(sample.data,
+         aes(x=date.time,
+             y=feet.below.surface,
+             color=site.name)) +
+    geom_point() +
+    theme_bw()
 
-# determine min and max value of each site
-ok.min.max <-
-  ok.data %>%
-  group_by(site.code) %>%
-  summarize(min.feet.below.surface=min(feet.below.surface),
-            max.feet.below.surface=max(feet.below.surface))
+  sample.min.max <-
+    sample.data %>%
+    group_by(site.code) %>%
+    summarize(min.feet.below.surface=min(feet.below.surface),
+              max.feet.below.surface=max(feet.below.surface))
+  
+  sample.data <-
+    tbl_df(merge(sample.data, sample.min.max, by='site.code'))
 
-ok.data <-
-  tbl_df(merge(ok.data, ok.min.max, by='site.code'))
+  sample.data$normalized.value <- NULL
+  sample.data <-
+    mutate(sample.data,
+           normalized.value =
+             (feet.below.surface - min.feet.below.surface) /
+             (max.feet.below.surface - min.feet.below.surface))
 
-ok.data$normalized.value <- NULL
-ok.data <-
-  mutate(ok.data,
-         normalized.value =
-           (feet.below.surface - min.feet.below.surface) /
-           (max.feet.below.surface - min.feet.below.surface))
+  ggplot(sample.data,
+         aes(x=date.time,
+             y=normalized.value,
+             color=site.name)) +
+    geom_point() +
+    theme_bw()
+}
 
-# determine center of the map that we need
-#
-# http://en.wikipedia.org/wiki/Geographic_center_of_the_contiguous_United_States
-center.lat = 39 + 50/60
-center.lon = -(98 + 35/60)
 
-# hmmm.... I don't like theirs. A little bitty
-# tweak to see if we can keep zoom=4 and get most
-# data points
-center.lat = 39.83
-center.lon = -96.50
-
-map <- get_map(location=c(lon=center.lon,
-                          lat=center.lat),
-               zoom=4)
-
-# TODO:
-#
-# figure out how to chunk up the data by date
-# so that we can draw a sequence of plots showing
-# water level changes over time
-
-ggmap(map, extent='panel') +
-  geom_point(data=ok.data,
-             aes(x=site.longitude,
-                 y=site.latitude,
-                 color=feet.below.surface))
+if (FALSE) {
+  # get sites that have at least a year's
+  # worth of data
+  ok.sites <-
+    water.data.df %>%
+    group_by(site.code) %>%
+    summarize(date.range=as.numeric(max(date.time) - min(date.time))) %>%
+    filter(date.range >= 365)
+  
+  # get the water data for those sites
+  ok.data <-
+    water.data.df %>%
+    filter(site.code %in% ok.sites$site.code) %>%
+    filter(feet.below.surface <= 3000.0)
+  
+  # determine min and max value of each site
+  ok.min.max <-
+    ok.data %>%
+    group_by(site.code) %>%
+    summarize(min.feet.below.surface=min(feet.below.surface),
+              max.feet.below.surface=max(feet.below.surface))
+  
+  ok.data <-
+    tbl_df(merge(ok.data, ok.min.max, by='site.code'))
+  
+  ok.data$normalized.value <- NULL
+  ok.data <-
+    mutate(ok.data,
+           normalized.value =
+             (feet.below.surface - min.feet.below.surface) /
+             (max.feet.below.surface - min.feet.below.surface))
+  
+  # determine center of the map that we need
+  #
+  # http://en.wikipedia.org/wiki/Geographic_center_of_the_contiguous_United_States
+  center.lat = 39 + 50/60
+  center.lon = -(98 + 35/60)
+  
+  # hmmm.... I don't like theirs. A little bitty
+  # tweak to see if we can keep zoom=4 and get most
+  # data points
+  center.lat = 39.83
+  center.lon = -96.50
+  
+  map <- get_map(location=c(lon=center.lon,
+                            lat=center.lat),
+                 zoom=4)
+  
+  numberOfDaysInMonth <- function(date) {
+    m <- format(date, format="%m")
+    
+    while (format(date, format="%m") == m) {
+      date <- date + 1
+    }
+    
+    return(as.integer(format(date - 1, format="%d")))
+  }
+  
+  # TODO:
+  #
+  # figure out how to chunk up the data by date
+  # so that we can draw a sequence of plots showing
+  # water level changes over time
+  START_YEAR <- 2010
+  END_YEAR <- 2014
+  OUTPUT_FNAME <- 'groundwater.gif'
+  
+  ani.options(outdir = getwd())
+  
+  # need to figure out how to create a
+  # rolling data set
+  
+  if (file.exists(OUTPUT_FNAME)) {
+    file.remove(OUTPUT_FNAME)
+  }
+  
+  saveGIF({
+    for (yr in START_YEAR:END_YEAR) {
+      for (mo in 1:12) {
+        day.1 = 1
+        
+        dt.1 <- as.Date(sprintf('%d-%s-%s',
+                                yr,
+                                formatC(mo, width=2, format='d', flag='0'),
+                                formatC(day.1, width=2, format='d', flag='0')))
+        
+        dt.2 <- (seq(dt.1, length=2, by="months")-1)[2]
+        
+        month.data <-
+          ok.data %>%
+          filter(date.time >= dt.1 & date.time <= dt.2)
+        
+        if (nrow(month.data) > 0) {
+          month.agg <-
+            month.data %>%
+            group_by(site.code, site.longitude, site.latitude) %>%
+            summarize(median.normalized.value=median(normalized.value))
+          
+          p <- ggmap(map) +
+            geom_point(data=month.agg,
+                       aes(x=site.longitude,
+                           y=site.latitude,
+                           color=median.normalized.value)) +
+            geom_text(aes(x=-120.0, y=20.0,
+                          label=paste0(month.name[mo], ' ', yr),
+                          size=20))
+          
+          print(p)
+        }
+      }
+    }
+  },
+  movie.name=OUTPUT_FNAME,
+  interval = 0.5,
+  ani.width = 1024, 
+  ani.height = 768
+  )
+}
